af6ec7ca804b4ca470db40f52aba2ee9fc8f7398
Blake Frederick <bf@six12creative.com>
2019-11-29 16:13:41 -0800

    Soundbar has been Marioized

--- TouchBarBar/WindowController.swift
+++ TouchBarBar/WindowController.swift
@@ -3,20 +3,26 @@
 //  TouchBarBar
 //
 //  Created by Guidouil on 10/29/16.
-//  Copyright © 2016 Guidouil. All rights reserved.
+//  Modified by Blake Frederick on 11/28/19.
 //
 
 import Cocoa
 
-fileprivate extension NSTouchBarCustomizationIdentifier {
-    static let touchBar = NSTouchBarCustomizationIdentifier("com.guidouil.touchbarbar")
+@available(OSX 10.12.2, *)
+fileprivate extension NSTouchBar.CustomizationIdentifier {
+    static let touchBar = NSTouchBar.CustomizationIdentifier("com.blakefrederick.touchbarbar")
 }
 
-fileprivate extension NSTouchBarItemIdentifier {
-    static let champagne = NSTouchBarItemIdentifier("champagne")
-    static let cocktail = NSTouchBarItemIdentifier("cocktail")
-    static let beer = NSTouchBarItemIdentifier("beer")
-    static let martini = NSTouchBarItemIdentifier("martini")
+@available(OSX 10.12.2, *)
+fileprivate extension NSTouchBarItem.Identifier {
+    static let oof = NSTouchBarItem.Identifier("")
+    static let longjump = NSTouchBarItem.Identifier("longjump")
+    static let longjumpover9000 = NSTouchBarItem.Identifier("longjumpover9000")
+    static let chainchomp = NSTouchBarItem.Identifier("chainchomp")
+    static let champagne = NSTouchBarItem.Identifier("champagne")
+    static let cocktail = NSTouchBarItem.Identifier("cocktail")
+    static let beer = NSTouchBarItem.Identifier("beer")
+    static let martini = NSTouchBarItem.Identifier("martini")
 }
 
 class WindowController: NSWindowController, NSTouchBarDelegate {
@@ -25,48 +31,67 @@ class WindowController: NSWindowController, NSTouchBarDelegate {
         // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
     }
 
-    func Isaac(sender: NSButton) {
+    @objc func Soundboard(sender: NSButton) {
         let title = sender.title
         
-        guard let sound = NSSound(named: title) else {
+        guard let sound = NSSound(named: NSSound.Name(rawValue: title)) else {
             return
         }
         sound.play()
     }
 
-    @available(OSX 10.12.1, *)
+    @available(OSX 10.12.2, *)
     override func makeTouchBar() -> NSTouchBar? {
         let touchBar = NSTouchBar()
         touchBar.delegate = self
         touchBar.customizationIdentifier = .touchBar
-        touchBar.defaultItemIdentifiers = [.champagne, .cocktail, .beer, .martini]
+        touchBar.defaultItemIdentifiers = [.oof, .longjump, .longjumpover9000, .chainchomp, .champagne, .cocktail, .beer, .martini]
         
         return touchBar
     }
     
-    @available(OSX 10.12.1, *)
-    func touchBar(_ touchBar: NSTouchBar, makeItemForIdentifier identifier: NSTouchBarItemIdentifier) -> NSTouchBarItem? {
+    @available(OSX 10.12.2, *)
+    func touchBar(_ touchBar: NSTouchBar, makeItemForIdentifier identifier: NSTouchBarItem.Identifier) -> NSTouchBarItem? {
         let touchBarItem = NSCustomTouchBarItem(identifier: identifier)
         switch identifier {
-        case NSTouchBarItemIdentifier.champagne:
-            let button = NSButton(title: "🍾", target: self, action: #selector(Isaac))
+        case NSTouchBarItem.Identifier.oof:
+            let buttonImage = NSImage(imageLiteralResourceName: "oof")
+            let button = NSButton(title: "oof", image: buttonImage, target: self, action: #selector(Soundboard))
             touchBarItem.view = button
             return touchBarItem
-            
-        case NSTouchBarItemIdentifier.beer:
-            let button = NSButton(title: "🍺", target: self, action: #selector(Isaac))
+        case NSTouchBarItem.Identifier.longjump:
+            let buttonImage = NSImage(imageLiteralResourceName: "longjump")
+            let button = NSButton(title: "longjump", image: buttonImage, target: self, action: #selector(Soundboard))
             touchBarItem.view = button
             return touchBarItem
-        case NSTouchBarItemIdentifier.cocktail:
-            let button = NSButton(title: "🍹", target: self, action: #selector(Isaac))
+        case NSTouchBarItem.Identifier.longjumpover9000:
+            let buttonImage = NSImage(imageLiteralResourceName: "longjumpover9000")
+            let button = NSButton(title: "longjumpover9000", image: buttonImage, target: self, action: #selector(Soundboard))
             touchBarItem.view = button
             return touchBarItem
-        case NSTouchBarItemIdentifier.martini:
-            let button = NSButton(title: "🍸", target: self, action: #selector(Isaac))
+        case NSTouchBarItem.Identifier.chainchomp:
+            let buttonImage = NSImage(imageLiteralResourceName: "chainchomp")
+            let button = NSButton(title: "chainchomp", image: buttonImage, target: self, action: #selector(Soundboard))
+            touchBarItem.view = button
+            return touchBarItem
+        case NSTouchBarItem.Identifier.champagne:
+            let button = NSButton(title: "🍾", target: self, action: #selector(Soundboard))
+            touchBarItem.view = button
+            return touchBarItem
+        case NSTouchBarItem.Identifier.beer:
+            let button = NSButton(title: "🍺", target: self, action: #selector(Soundboard))
+            touchBarItem.view = button
+            return touchBarItem
+        case NSTouchBarItem.Identifier.cocktail:
+            let button = NSButton(title: "🍹", target: self, action: #selector(Soundboard))
+            touchBarItem.view = button
+            return touchBarItem
+        case NSTouchBarItem.Identifier.martini:
+            let button = NSButton(title: "😬", target: self, action: #selector(Soundboard))
             touchBarItem.view = button
             return touchBarItem
         default:
-            let button = NSButton(title: "🍹", target: self, action: #selector(Isaac))
+            let button = NSButton(title: "🍹", target: self, action: #selector(Soundboard))
             touchBarItem.view = button
             return touchBarItem
         }
